f13129884dc6e75cbe44c3e49acda76439e21d0e
bonejjo <bonejjo@gmail.com>
2017-12-04 22:39:40 +0900

    save function&sidebar

--- todoList.js
+++ todoList.js
@@ -1,5 +1,22 @@
-$('.wizard icon').click(function() {
-    $('.ui.sidebar')
-        .sidebar('toggle')
-    ;
+function myFunction(){
+    var bar=$('.ui.sidebar');
+    bar.sidebar('toggle');
+}
+var titleList=new Array();
+var contextList=new Array();
+var i=0;
+function saveFunction(){
+        var title=$('#title').val();
+        var context=$('#context').val();
+        titleList.push(title);
+        contextList.push(context);
+        $('.content').append("<li id="+i+">"+titleList[i]+" <i class=\"empty star icon\"></i></li>");
+        i++;
+}
+$(function(){
+    var star=$("i.empty.star.icon");
+    $("li").delegate(star,"click", function(){
+        $(this).css('background-color', 'yellow');
+    });
 });
+
