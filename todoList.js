dfe40a3ef0206e7d10f979e09720119edde3b424
bonejjo <bonejjo@gmail.com>
2017-12-05 14:22:55 +0900

    new button&emphasis

--- todoList.js
+++ todoList.js
@@ -1,22 +1,26 @@
+var titleList=new Array();
+var contextList=new Array();
+var i=0;
 function myFunction(){
     var bar=$('.ui.sidebar');
     bar.sidebar('toggle');
 }
-var titleList=new Array();
-var contextList=new Array();
-var i=0;
 function saveFunction(){
-        var title=$('#title').val();
-        var context=$('#context').val();
-        titleList.push(title);
-        contextList.push(context);
-        $('.content').append("<li id="+i+">"+titleList[i]+" <i class=\"empty star icon\"></i></li>");
-        i++;
+    var title=$('#title').val();
+    var context=$('#context').val();
+    titleList.push(title);
+    contextList.push(context);
+    $('.content').append("<li id="+i+">"+titleList[i]+" <i class=\"empty star icon\"></i></li>");
+    i++;
 }
-$(function(){
-    var star=$("i.empty.star.icon");
-    $("li").delegate(star,"click", function(){
-        $(this).css('background-color', 'yellow');
-    });
+$(document).on('click','i.empty.star.icon',function(){
+    $(this).parent().css('background-color','yellow');
 });
+$(document).on('click','i.write.icon',function(){
+    var bar=$('.ui.sidebar');
+    bar.sidebar('hide');
+    $('#title').val('');
+    $('#context').val('');
+});
+
 
